571e4f79657833f597ada998128fc94a1300f6e1
radovanradic <radicr@gmail.com>
2022-11-29 10:37:47 +0100

    Upgrading mn core to 3.7.4

--- data-mongodb/src/test/groovy/io/micronaut/data/document/mongodb/reactive/MongoReactiveDocumentRepositorySpec.groovy
+++ data-mongodb/src/test/groovy/io/micronaut/data/document/mongodb/reactive/MongoReactiveDocumentRepositorySpec.groovy
@@ -3,4 +3,14 @@ package io.micronaut.data.document.mongodb.reactive
 import io.micronaut.data.document.mongodb.MongoDocumentRepositorySpec
 
 class MongoReactiveDocumentRepositorySpec extends MongoDocumentRepositorySpec implements MongoSelectReactiveDriver {
+
+    void "test save and load one"() {
+        given:
+        savePersons(["User1"])
+        when:
+        def person = personRepository.findByName("User1")
+        then:
+        person
+        person.name == "User1"
+    }
 }
